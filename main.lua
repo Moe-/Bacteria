6a528817e8c21c89e0bc802e51a299ab8a5efd17
ghoulsblade <ghoulsblade@schattenkind.net>
2013-01-25 21:13:39 +0100

    escape quit + conf

--- main.lua
+++ main.lua
@@ -7,3 +7,8 @@ function love.draw ()
 
 	love.graphics.print("hello world",40,40)
 end
+
+function love.keypressed (keyname)
+	if (keyname == "escape") then love.event.quit( ) end
+	print("keypress",keyname)
+end
