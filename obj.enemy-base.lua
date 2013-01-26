ff148abcc1df1ea22463d881f0e780d83076e0cf
ghoulsblade <ghoulsblade@schattenkind.net>
2013-01-26 16:18:13 +0100

    enemies kept on screen

--- obj.enemy-base.lua
+++ obj.enemy-base.lua
@@ -3,7 +3,12 @@ cEnemyBase = CreateClass(cBase)
 gEnemies = {}
 
 function Enemies_Update (dt) 
+	local w = love.graphics.getWidth()
+	local h = love.graphics.getHeight()
+	
 	for o,_ in pairs(gEnemies) do 
+		o.x = max(0,min(w,o.x))
+		o.y = max(0,min(h,o.y))
 		o:Update(dt)
 		o:CheckCollisionWithPlayer(dt)
 	end 
