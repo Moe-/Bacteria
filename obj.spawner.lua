90eebd866aa51e2691327d7039f46738f0bd4dce
Rene Bruns <webmaster@renebruns.de>
2013-01-26 12:11:01 +0100

    Boss spawn / Weapon spawn

--- obj.spawner.lua
+++ obj.spawner.lua
@@ -1,7 +1,6 @@
 cSpawner = CreateClass()
 
 function cSpawner:Init()
-    print "Spawm me"
     self.formations = {
                     {
                        {enemy = "blutplatt",offsetX = 80, offsetY = 80},
@@ -44,8 +43,33 @@ function cSpawner:spawnFormation()
     end
 end
 
+function cSpawner:spawnWeapons()
+    local rand = math.random(1,100)
+    if (rand == 1) then
+        local w = love.graphics.getWidth()
+        local h = love.graphics.getHeight()
+
+        print "Spawning weapon"
+        cEnemyWeapon:New(0.9*w,randf()*h, rand_in_arr({"red", "green", "blue", "white"}))
+    end
+end
+
+function cSpawner:EnemiesOnScreen()
+    local found = false
+    for k,v in pairs(gEnemies) do
+        if ((v == true) and (k.enemy_kind ~= "weapon")) then
+            found = true
+            break
+        end
+    end
+    return found
+end
+
+
 function cSpawner:Update()
-    if (count(gEnemies) < 1) then
+    self:spawnWeapons()
+
+    if (self:EnemiesOnScreen() == false) then
         self:spawnFormation()
     end
 end
