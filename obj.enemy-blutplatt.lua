258aa288ca7179b38dfb7ebdfdc9c0b517e50f35
Markus Brenner <markus.brenner@uni-ulm.de>
2013-01-26 08:46:43 +0100

    Added movement to bluttplatt

--- obj.enemy-blutplatt.lua
+++ obj.enemy-blutplatt.lua
@@ -6,6 +6,32 @@ function cEnemyBlutPlatt:Init(x,y)
 	self.energy = 120
 	self.gfx = gfx_blutplatt
 	self:Register()
+	
+	self.xdir = math.random(50, 150)
+	self.ydir = math.random(-50, 50)
 end
 
 function cEnemyBlutPlatt:Draw() self:DrawWobble(0.2,0.1,gEnemyGfxScale) end
+
+function cEnemyBlutPlatt:Update(dt)
+	self.x = self.x - self.xdir * dt
+	self.y = self.y - self.ydir * dt
+	
+	if self.x < 0 then
+		self.x = 0
+		self.xdir = -self.xdir
+	elseif self.x > 1024 then
+		self.x = 1024
+		self.xdir = -self.xdir
+	end
+	
+	if self.y < 0 then
+		self.y = 0
+		self.ydir = -self.ydir
+	elseif self.y > 768 then
+		self.y = 768
+		self.ydir = -self.ydir
+	end
+	
+	
+end
