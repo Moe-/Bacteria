32f1f2db4a1d47fb3ca948e58914d2a696eef0dd
Markus Brenner <markus.brenner@uni-ulm.de>
2013-01-26 01:40:59 +0100

    Moved Effect scripts to main folder

---
+++ obj.EffectBasic.lua
@@ -0,0 +1,25 @@
+cEffectBasic = CreateClass()
+
+function cEffectBasic:Init(sprite, x, y)
+	self.ps = cPartSys:New()	
+	self.posx = x
+	self.posy = y
+	self.spr = sprite
+end
+
+function cEffectBasic:Draw()
+	self.ps:Draw(self.posx, self.posy)
+end
+
+function cEffectBasic:Update(dt)
+	self.ps:Update(dt)
+end
+
+function cEffectBasic:isDone()
+	return self.ps:isDone()
+end
+
+function cEffectBasic:Free()
+	self.ps:Stop()
+	self.ps:Reset()
+end
